1d5da81aa893dd051937f0476afa661bb52af41b
Victoria B <viktorija.bourassa@gmail.com>
2019-01-28 16:20:56 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,19 @@
 class Author
   attr_accessor :name
   @posts = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
 end
 
 
-class Artist
-  attr_accessor :name
-  @songs = []
-  def initialize (name)
-    @name = name
-  end
+
 
   def add_song(song)
     @songs << song
